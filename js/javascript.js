0774e2452efa8b6efe4968b3a5f0320900d5ee54
OlegikSSoN <olegiksson@gmail.com>
2023-01-22 11:40:05 +0200

    JavaScript

---
+++ js/javascript.js
@@ -0,0 +1,28 @@
+const name = prompt("Name");
+
+if (name == 'Vlad') {
+    console.log("Hello Vlad")
+} else {
+    console.log("You is no Vlad")
+}
+
+
+//const age = prompt("Вік");
+//const topcolor = prompt("Улюблений колір");
+//const haircolor = prompt("Колір волосся");
+
+//const result = `Привіт тобі ${age}, твій улюблений колір ${topcolor}, колір твого волосся ${haircolor}`
+//alert(result)
+
+//console.log(document)
+//const age = prompt('Введіть вік')
+//const name = prompt("Ім'я")
+//const surname = prompt('Введіть прізвище')
+//console.log("age = ", age)
+
+//alert("Привіт " + name, " " + surname)
+//const user = {
+//    firstname: name;
+//    surname: surname;
+//    age: age
+//}
